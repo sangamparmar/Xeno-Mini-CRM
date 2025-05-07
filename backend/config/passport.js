c107e32697bc8aa39004b7bd676da87f5dd9dc8b
Sangam Parmar <118618529+sangamparmar@users.noreply.github.com>
2025-05-07 23:33:42 +0530

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user.model');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
